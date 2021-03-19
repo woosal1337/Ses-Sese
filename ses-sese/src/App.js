f19c40950972bb0625059ebbcc0d17bfcc2fd656
Vusal Ismayilov <woosal@protonmail.com>
2021-03-20 02:40:20 +0300

    5.0

--- ses-sese/src/App.js
+++ ses-sese/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "./index.css"
+import fire from "./fire"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {}
+        this.buttonHandle = this.buttonHandle.bind(this)
+    }
+
+    buttonHandle(event) {
+        event.preventDefault()
+        if (event.target.id === "paster") {
+            fire.database().ref('paster').push(1);
+        } else {
+            fire.database().ref('ruzgar').push(1);
+        }
+
+        console.log(event.target.id)
+    }
+
+    render() {
+        return (
+            <div>
+                <header className="v-header container">
+                    <div className="card">
+                        <div className="card_image"><img
+                            src="https://pbs.twimg.com/profile_images/1210888068951097345/exveNLDs.jpg"/></div>
+                        <div className="card_title title-white">
+                            <button id="paster" className="a3" onClick={this.buttonHandle}>Vote!</button>
+                        </div>
+                    </div>
+
+                    <img src="https://woosal.com/1337/kk-background.png" className="kk-image"/>
+
+                    <div className="card">
+                        <div className="card_image"><img
+                            src="https://yt3.ggpht.com/ytc/AAUvwnjk04rBskmG9iHDFh9tS5nFU9Kmbrk0xRmWpukUOQ=s1024-c-k-c0x00ffffff-no-rj"/>
+                        </div>
+                        <div className="card_title title-white">
+                            <button id="ruzgar" className="a3" onClick={this.buttonHandle}>Vote!</button>
+                        </div>
+                    </div>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
